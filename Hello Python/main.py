530811347b2fe3f146ee92b68ad603b443c47566
PO-CHUN CHEN <>
2023-07-03 04:11:48 +0800

    Hello Python

---
+++ Hello Python/main.py
@@ -0,0 +1,12 @@
+# Hello Python
+
+#Function to print Hello World
+def print_hi(name):
+    print(f'Hi, {name}')
+    print("Hello World")
+
+
+# Main function
+if __name__ == '__main__':
+   print_hi('JC666')
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# Python Learn
+
+## 專案文件說明
+1.Hello World
+
+
+
